d74a473e265030d3f136a3f6e2afe33098e79482
Drikus Roor <drikusroor@gmail.com>
2017-07-13 18:48:34 +0200

    Added actual loading of scene when clicking button and made a back button

--- 04 Number Wizard UI/Assets/LevelManager.cs
+++ 04 Number Wizard UI/Assets/LevelManager.cs
@@ -5,6 +5,7 @@ public class LevelManager : MonoBehaviour {
 
     public void LoadLevel(string name)
     {
+        Application.LoadLevel(name);
         Debug.Log("Level load requested for " + name);
     }
 
