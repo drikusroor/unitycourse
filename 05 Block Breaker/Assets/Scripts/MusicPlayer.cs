89a95e4f534603ab26e9d1e00e5cd2822b2e569d
Drikus Roor <drikusroor@gmail.com>
2017-07-14 23:00:48 +0200

    Edited MusicPlayer class to only allow one instance at the same time

--- 05 Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ 05 Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+    public static int musicPlayerCount = 0;
+
 	// Use this for initialization
 	void Start () {
-	
+        GameObject.DontDestroyOnLoad(gameObject);
+        if (MusicPlayer.musicPlayerCount > 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        MusicPlayer.musicPlayerCount += 1;
 	}
 	
 	// Update is called once per frame
