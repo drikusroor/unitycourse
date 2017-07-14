c70ecc00ac7ae410aeeeb9ea82e1cc7d25e1c686
Drikus Roor <drikusroor@gmail.com>
2017-07-14 23:23:11 +0200

    Moved duplicate destroying to Awake method

--- 05 Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ 05 Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -5,14 +5,19 @@ public class MusicPlayer : MonoBehaviour {
 
     public static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
         if (instance != null)
         {
             GameObject.Destroy(gameObject);
         }
         instance = this;
         GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
