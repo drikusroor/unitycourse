61e260d5622b3ccf2f570769481ec5a4d86ea232
Drikus Roor <drikusroor@gmail.com>
2017-07-14 22:43:26 +0200

    Added font, music and made levels ready for next phase of the game

---
+++ 05 Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Application.LoadLevel(name);
+        Debug.Log("Level load requested for " + name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ 05 Block Breaker/Assets/Scripts/PlayTada.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayTada : MonoBehaviour {
+
+    public AudioSource tada;
+
+	// Use this for initialization
+	void Start () {
+        tada.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
