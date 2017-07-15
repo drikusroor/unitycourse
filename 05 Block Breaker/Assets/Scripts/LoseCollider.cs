1318ae0278cdccfcf9ff2e172474e0154d3df9b2
Drikus Roor <drikusroor@gmail.com>
2017-07-15 11:28:18 +0200

    Added sprites, background, started working on colliders

---
+++ 05 Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+}
