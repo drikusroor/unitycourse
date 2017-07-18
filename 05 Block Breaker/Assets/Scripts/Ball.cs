4ae767363a19ed641c00ce5ab6615cf5407e7f47
Drikus Roor <drikusroor@gmail.com>
2017-07-18 19:32:08 +0200

    Made ball more bouncy again, made gravity uniform. Added click event to start moving the ball.

---
+++ 05 Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+
+    private Vector3 paddleToBallVector;
+
+    private bool gameStarted = false;
+
+	// Use this for initialization
+	void Start () {
+
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameStarted)
+        {
+            // Lock ball to paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            // Wait for mouse press to start game
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.rigidbody2D.velocity = new Vector2 (3f, 10f);
+                gameStarted = true;
+            }
+        }
+
+        
+	}
+}
