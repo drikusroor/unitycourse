11383d316fc50731868f4b21341ec03ca8430285
Drikus Roor <drikusroor@gmail.com>
2017-08-07 22:49:15 +0200

    Added player ship sprite and player controlled movement

---
+++ 06 Laser Defender/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleControl ();	
+	}
+
+	void HandleControl() {
+		Vector3 pos = transform.position;
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position = new Vector3 (pos.x - moveSpeed, pos.y);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position = new Vector3 (pos.x + moveSpeed, pos.y);
+		}
+
+	}
+}
