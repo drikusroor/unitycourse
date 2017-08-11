a72f95fd60ab9af62c369f204ca051160e69b8b1
Drikus Roor <drikusroor@gmail.com>
2017-08-11 21:43:32 +0200

    Restricted movement of player according to viewport

--- 06 Laser Defender/Assets/Scripts/PlayerShip.cs
+++ 06 Laser Defender/Assets/Scripts/PlayerShip.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class PlayerShip : MonoBehaviour {
 
 	public float moveSpeed;
+	public float padding = 1f;
+	float xmin = -5f;
+	float xmax = 5f;
 
 	// Use this for initialization
 	void Start () {
+		SetPlayerBoundaries ();
+	}
+
+	void SetPlayerBoundaries()
+	{
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
 	}
 	
 	// Update is called once per frame
@@ -18,10 +31,14 @@ public class PlayerShip : MonoBehaviour {
 	void HandleControl() {
 		Vector3 pos = transform.position;
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3 (-moveSpeed * Time.deltaTime, 0f);
+			transform.position += Vector3.left * moveSpeed * Time.deltaTime;
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3 (moveSpeed * Time.deltaTime, 0f);
+			transform.position += Vector3.right * moveSpeed * Time.deltaTime;
 		}
+
+		// Restrict player to game space
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 
 	}
 }
