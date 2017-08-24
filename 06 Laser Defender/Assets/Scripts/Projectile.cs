73f1e2bb966fb8c04c90febb0326b49ce061a3b1
Drikus Roor <drikusroor@gmail.com>
2017-08-24 20:23:27 +0200

    Added scripts for the projectile and enemy and made a collision detection

---
+++ 06 Laser Defender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float damage = 100f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
