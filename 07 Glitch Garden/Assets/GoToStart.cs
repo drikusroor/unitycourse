a6fed90000879773cff96bf62f04217b06c29c64
Drikus Roor <drikusroor@gmail.com>
2017-09-19 22:03:52 +0200

    Added splash screen & sound and a redirect to start menu after 5s

---
+++ 07 Glitch Garden/Assets/GoToStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("GoToStartScene", 5);
+	}
+
+	void GoToStartScene()
+	{
+		LevelManager levelManager = new LevelManager ();
+
+		levelManager.LoadLevel("Start Menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 07 Glitch Garden/Assets/SplashImageDelay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashImageDelay : MonoBehaviour {
+
+	private 
+
+	// Use this for initialization
+	void Start () {
+		transform.gameObject.SetActive (false);
+		Invoke ("ShowSplashImage", 1);
+	}
+
+	void ShowSplashImage()
+	{
+		transform.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
