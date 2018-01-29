7b47b6d8f8a23c0d8ffdf16798e4db504e876518
Drikus Roor <drikusroor@gmail.com>
2018-01-29 20:44:22 +0100

    Re-init after long break

---
+++ 07 Glitch Garden/Assets/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("GoToStartScene", 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GoToStartScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
